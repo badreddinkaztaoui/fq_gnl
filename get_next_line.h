f5e884ab56dcea98b4a5bd3d62b01b3d8d5ff1bc
badreddinkaztaoui <kaztaouibadreddin@gmail.com>
2023-05-02 12:14:18 +0100

    Add project files

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badreddin <badreddin@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 05:27:23 by badreddin         #+#    #+#             */
+/*   Updated: 2023/04/13 23:37:56 by badreddin        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdint.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+size_t	ft_strlen(char *str);
+char	*newline_exist(char *str);
+char	*ft_strjoin(char *stash, char *buff);
+void	*ft_calloc(size_t count, size_t size);
+char	*get_next_line(int fd);
+
+#endif
